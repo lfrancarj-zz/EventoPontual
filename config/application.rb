aff9eb4be4564f5212f1330095da06245ca05c06
Leandro França <leandro.franca@pobox.com>
2015-02-23 21:08:26 +0000

    After Assets precompile 5 - for Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module Eventopontual
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.i18n.default_locale = :'pt-BR'
+    config.assets.initialize_on_precompile = false
 
   end
 end
